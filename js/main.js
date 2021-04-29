965ecad524cac1ebcbd91f4b898537db78b771a2
Laucha Carro <lautaroecarro@gmail.com>
2021-04-29 16:49:19 -0300

    Agrego validaciones de campos

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function () {
         headerTag: "h2",
         bodyTag: "section",
         transitionEffect: "fade",
-        enableAllSteps: true,
+        enableAllSteps: false,
         stepsOrientation: "vertical",
         autoFocus: true,
         saveState: true,
@@ -15,8 +15,69 @@ $(function () {
             finish: '<i class="zmdi zmdi-check"></i>',
             current: ''
         },
+        onStepChanging: function (event, currentIndex, newIndex) {
+            if (currentIndex == 1 && newIndex == 2)
+                return validarInformacionPersonal();
+
+            if (currentIndex == 2 && newIndex == 3)
+                return validarPresentacion();
+
+            return true;
+        },
     })
 });
+
+function validarInformacionPersonal() {
+    let name = document.getElementById("first-name").value;
+    let lastName = document.getElementById("last-name").value;
+    let email = document.getElementById("your_email").value;
+    let description = document.getElementById("description").value;
+    let image = document.getElementById("image").value;
+
+    let result = true;
+
+    const errorHandle = () => result = false;
+
+    validateInput(name, "name-fieldset", errorHandle);
+    validateInput(lastName, "lastname-fieldset", errorHandle);
+    validateInput(ValidateEmail(email), "email-fieldset", errorHandle);
+    validateInput(description, "description-fieldset", errorHandle);
+    validateInput(image, "image-fieldset", errorHandle);
+
+    return result;
+}
+
+function validarPresentacion() {
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description2").value;
+
+    let result = true;
+
+    const errorHandle = () => result = false;
+
+    validateInput(title, "title-fieldset", errorHandle);
+    validateInput(description, "description2-fieldset", errorHandle);
+
+    return result;
+}
+
+
+
+function validateInput(condition, fieldsetId, errorCallback) {
+    var fieldset = document.getElementById(fieldsetId)
+    if (condition) {
+        if (fieldset.classList.contains("error")) {
+            fieldset.classList.remove("error");
+        }
+    }
+    else {
+        if (!fieldset.classList.contains("error")) {
+            fieldset.classList.add("error");
+        }
+        errorCallback()
+    }
+}
+
 
 function mesOnChange() {
     let selectMes = document.getElementById("month");
@@ -47,4 +108,11 @@ function mesOnChange() {
         selectDia.add(option);
 
     }
+}
+
+function ValidateEmail(email) {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
+        return true
+    }
+    return false
 }
