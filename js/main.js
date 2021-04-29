6257448814a9860f9f3e39231c8493747a3a9653
Laucha Carro <lautaroecarro@gmail.com>
2021-04-29 17:37:51 -0300

    agrego metodo para leer la imagen

--- js/main.js
+++ js/main.js
@@ -116,3 +116,11 @@ function ValidateEmail(email) {
     }
     return false
 }
+
+//https://stackoverflow.com/questions/36280818/how-to-convert-file-to-base64-in-javascript
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
