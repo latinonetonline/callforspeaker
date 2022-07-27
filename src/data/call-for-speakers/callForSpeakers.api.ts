fc42c0ae59468a258054fdb252383c40529f4d83
Laucha Carro <lautaroecarro@gmail.com>
2022-07-27 02:37:21 -0300

    Cambio url

--- src/data/call-for-speakers/callForSpeakers.api.ts
+++ src/data/call-for-speakers/callForSpeakers.api.ts
@@ -2,7 +2,7 @@ import { config } from "../../config/EnvConfig";
 
 
 export const getUnavailableDates = async (): Promise<Date[]> => {
-  return await fetch(config.apiUrl + "/api/v1/callforspeakers-module/Proposals/dates")
+  return await fetch(config.apiUrl + "/api/v1/webinars-module/Proposals/dates")
   .then(data => data.json())
   .then(response => (response.result.dates as string[]).map(x => new Date(x)));
 };
